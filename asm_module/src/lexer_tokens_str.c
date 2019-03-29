78d9187c3d263eb919dc503a687af0571a0c704c
hbally <hbally@student.42.fr>
2019-03-29 17:02:48 +0100

    added lexer_tokens_str.c

---
+++ asm_module/src/lexer_tokens_str.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_tokens_str.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/29 16:14:00 by hbally            #+#    #+#             */
+/*   Updated: 2019/03/29 16:56:54 by hbally           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "types.h"
+#include "macros.h"
+#include "asm.h"
+
+static uint8_t		endofstr(char c)
+{
+	return (c == COMMENT_CHAR
+				|| c == LABEL_CHAR
+				|| c == DIRECT_CHAR
+				|| c == SEPARATOR_CHAR
+				|| c == QUOTE_CHAR
+				|| ft_iswhitespace(c));
+}
+
+static t_code		token_str(const t_toktype type,
+								t_line *line,
+								t_tokenlst *lst)
+{
+	t_token			template;
+	uint16_t		start;
+
+	template_init(&template, type, line);
+	start = (line->index)++;
+	while (line->str[line->index + 1] && !endofstr(line->str[line->index + 1]))
+	{
+		line->index++;
+		if (line->index - start > MAX_TOKEN_STR_SIZE)
+			return (error_handler(strsize, 0, line));
+	}
+	get_right_pad(&template, line);
+	template.value = ft_strsub(line->str, start, ++(line->index) - start);
+	if (!(template.value))
+		return (error_handler(malloc, 0, 0));
+	else
+		return (token_add(lst, &template));
+}
+
+t_code				token_str_wrapper(const t_toktype type,
+										t_line *line,
+										t_tokenlst *lst)
+{
+	if (ft_iswhitespace(line->str[line->index]))
+		line->index++;
+	else if (line->str[line->index] == '#')
+	{
+		while (line->str[line->index])
+			line->index++;
+	}
+	else
+		return (token_string(type, line, lst));
+	return (done);
+}
