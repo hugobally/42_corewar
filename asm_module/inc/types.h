45707ca08e48fd8aba75851d8211dc81da4d2a53
hbally <hbally@student.42.fr>
2019-03-29 17:00:17 +0100

    changed types.h

--- asm_module/inc/types.h
+++ asm_module/inc/types.h
@@ -6,28 +6,64 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 14:56:40 by hbally            #+#    #+#             */
-/*   Updated: 2019/03/28 19:26:48 by hbally           ###   ########.fr       */
+/*   Updated: 2019/03/29 16:37:01 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
 
+# include <stdint.h>
+# include "macros.h"
+# include "libcorewar.h"
+
 /*
-** Special characters not defined in op.h
+** Line Information Container
 */
 
-# define QUOTE_CHAR '"'
-
+typedef struct		s_line
+{
+	char			*str;
+	uint16_t		num;
+	uint16_t		index;
+}					t_line;
 /*
-** Padding Flags
+** Tokens
 */
 
-# define PAD_RIGHT 1
-# define PAD_LEFT 2
+typedef enum		e_toktype
+{
+	value_string,
+	value_label,
+	value_opcode,
+	value_reg,
+	value_number,
+	char_dir = DIRECT_CHAR,
+	char_label = LABEL_CHAR,
+	char_sep = SEPARATOR_CHAR,
+	char_eol = EOL_CHAR,
+	quote = QUOTE_CHAR,
+	cmd,
+}					t_toktype;
+
+typedef struct		s_token
+{
+	char			*value;
+	t_toktype		type;
+	uint32_t		pos;
+	uint8_t			pad;
+	struct s_token	*next;
+}					t_token;
+
+typedef struct		s_tokenlst
+{
+	t_token			*start;
+	t_token			*end;
+	uint32_t		size;
+}					t_tokenlst;
 
 /*
-** Exit codes
+** Control Flow Enums
 */
 
 typedef enum		e_code
@@ -38,55 +74,23 @@ typedef enum		e_code
 
 typedef enum		e_errors
 {
-	malloc,
-	read,
-	no_error
+	malloc,//no token
+	read_crash,
+	read_linesize,
+	filesize,
+	strsize,//no token
+	err,
+	no_err
 }					t_errors;
 
 /*
-** Tokens
-*/
-
-typedef enum		e_toktype
-{
-	string,
-	opcode,
-	reg,
-	number,
-	comment, 
-	dir_c,
-	label_c,
-	separator_c,
-	quote_c,
-	eol_c,
-	namecmd_s,
-	commentcmd_s,
-}					t_toktype;
-
-typedef struct		s_token
-{
-	char			*value;
-	t_toktype		type;
-	uint64_t		pos;
-	uint8_t			pad;
-	struct s_token	*next;
-}					t_token;
-
-typedef struct		s_tokenlst
-{
-	t_token			*start;
-	t_token			*end;
-}					t_tokenlst;
-
-
-/*
-** Parsing Dispatcher __UNUSED__ ?
+** Dispatcher Table
 */
 
 typedef struct		s_dispatch
 {
-	char			character;
-	t_code			(*handler)(static char*, uint64_t, int16_t*, t_tokenlst*);
+	t_toktype		type;
+	t_code			(*handler)(t_toktype, t_line*, t_tokenlst*);
 }					t_dispatch;
 
 #endif
