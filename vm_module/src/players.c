06f5c87b1fb467eb6d580b5425f31938e79a581a
rle-ru <rle-ru@student.42.fr>
2019-03-29 10:37:59 +0100

    better error handling on .core opening & norme

--- vm_module/src/players.c
+++ vm_module/src/players.c
@@ -2,31 +2,24 @@
 #include <unistd.h>
 #include "corewar.h"
 
-void	push_player(t_core *core, t_player *new)
+void		push_player(t_core *core, t_player *new)
 {
 	new->next = core->players;
 	core->players = new;
 }
 
 /*
-** Reste a tout compartimenter et a close le fd en cas d'erreur.
 ** Changer MEM_SIZE ici par max(CHAMP_MAX_SIZE, PROG_NAME_LENGTH, COMMENT_LENGTH)
 */
 
-t_errors new_player(t_core *core, char *av)
+t_errors	core_read(int fd, t_player *new)
 {
-	t_player	*new;
-	char		buff[MEM_SIZE + 1];
-	int			fd;
+	char	buff[MEM_SIZE + 1];
 
 	ft_bzero(buff, MEM_SIZE + 1);
-	if (!(new = (t_player*)ft_memalloc(sizeof(t_player))))
-		return (falloc);
-	if ((fd = open(av, O_RDONLY)) < 2)
-		return (badopen);
 	if (read(fd, buff, 4) < 4)
 		return (badfile);
-	if ((int)buff[0] != 0xea83f3)
+	if ((int)buff[0] != COREWAR_EXEC_MAGIC)
 		return (badfile);
 	if (read(fd, buff, 128) < 128)
 		return (badfile);
@@ -45,6 +38,24 @@ t_errors new_player(t_core *core, char *av)
 	ft_memcpy(new->proc, buff, new->size);
 	if (read(fd, buff, MEM_SIZE) > 0)
 		return (badfile);
+	return (ok);
+}
+
+t_errors	new_player(t_core *core, char *av)
+{
+	t_player	*new;
+	int			fd;
+	t_errors	ret;
+
+	if (!(new = (t_player*)ft_memalloc(sizeof(t_player))))
+		return (falloc);
+	if ((fd = open(av, O_RDONLY)) < 2)
+		return (badopen);
+	if ((ret = core_read(fd, new)) != ok)
+	{
+		close(fd);
+		return (ret);
+	}
 	close(fd);
 	push_player(core, new);
 	return (ok);
