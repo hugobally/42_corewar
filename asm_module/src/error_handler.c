1ef10f5d21d894128a6f60d6556af14dc165f139
hbally <hbally@student.42.fr>
2019-03-29 17:02:06 +0100

    added error_handler.c

---
+++ asm_module/src/error_handler.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handler.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/29 13:20:35 by hbally            #+#    #+#             */
+/*   Updated: 2019/03/29 13:30:40 by hbally           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "types.h"
+
+t_code			error_handler(t_errors code, t_toktype *token, t_line *line)
+{
+	(void)code;
+	(void)token;
+	(void)line;
+	return (error);
+}
