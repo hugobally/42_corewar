a88363a54b0ccea86c37ed4f7328de8215d6da47
hbally <hbally@student.42.fr>
2019-03-29 17:01:56 +0100

    added macros.h

---
+++ asm_module/inc/macros.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   macros.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/29 10:11:45 by hbally            #+#    #+#             */
+/*   Updated: 2019/03/29 16:35:48 by hbally           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACROS_H
+# define MACROS_H
+
+/*
+** Special characters not defined in op.h
+*/
+
+# define QUOTE_CHAR 			'"'
+# define EOL_CHAR 				'\n'
+
+/*
+** Allowed characters in numerical value
+*/
+
+# define NUMERIC_CHARS			"-0123456789"
+/*
+** Padding Flags
+*/
+
+# define PAD_RIGHT 				1
+# define PAD_LEFT 				2
+
+/*
+** Size Restrictions
+** (Max is 10000 lines at 80 tokens / line without comments, max token length 512)
+*/
+
+# define MAX_TOKEN_LIST_SIZE 	10000 * 80
+# define MAX_TOKEN_STR_SIZE		512
+
+#endif
