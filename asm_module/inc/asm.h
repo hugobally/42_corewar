16aa649b0d7f97096151d01dc5f7ed35b65ab7d1
hbally <hbally@student.42.fr>
2019-03-30 11:26:34 +0100

    changed asm.h

--- asm_module/inc/asm.h
+++ asm_module/inc/asm.h
@@ -6,7 +6,7 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 13:29:36 by hbally            #+#    #+#             */
-/*   Updated: 2019/03/29 16:59:38 by hbally           ###   ########.fr       */
+/*   Updated: 2019/03/30 11:26:12 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 # define ASM_H
 
 # include "types.h"
+# include "string.h"
+
+/*
+**	DEBUG
+*/
+
+void			debug_printtokenlst(t_tokenlst *lst);//
 
 /*
 ** Lexer Pass 1
@@ -29,17 +36,16 @@ t_code			lexer_exit(t_tokenlst *lst, t_errors code);
 
 void			template_init(t_token *template, t_toktype type, t_line *line);
 void			get_right_pad(t_token *template, t_line *line);
-t_code			token_add(t_tokenlst *lst, const t_token *template);
+t_code			token_add(t_tokenlst *lst, const t_token *template,
+									const size_t value_size);
 t_code			token_single(const t_toktype type,
-								t_line *line,
-								t_tokenlst *lst);
-t_code			token_str(const t_toktype type,
-								t_line *line,
-								t_tokenlst *lst);
+								t_line *line, t_tokenlst *lst);
 t_code			token_str_wrapper(const t_toktype type,
-									t_line *line,
-									t_tokenlst *lst);
+									t_line *line, t_tokenlst *lst);
 t_code			token_newline(t_line *line, t_tokenlst *lst);
+t_code			token_quote(const t_toktype type,
+								t_line *line,
+								t_tokenlst *lst);
 
 /*
 ** Errors
