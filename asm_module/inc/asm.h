16831d4632649969f1c8e59fcb938f039cb3fe6e
hbally <hbally@student.42.fr>
2019-03-30 18:20:41 +0100

    asm | changed asm.h

--- asm_module/inc/asm.h
+++ asm_module/inc/asm.h
@@ -11,24 +11,18 @@
 void			debug_printtokenlst(t_tokenlst *lst);//
 
 /*
-** Lexer Pass 1
+** Lexer Scanner
 */
 
 t_code			lexer_scanner(const int fd);
 t_code			lexer_exit(t_tokenlst *lst, t_errors code);
-
-/*
-** Token Handling
-*/
-
-
 void			template_init(t_token *template, t_toktype type, t_line *line);
 void			get_right_pad(t_token *template, t_line *line);
 t_code			token_add(t_tokenlst *lst, const t_token *template,
 									const size_t value_size);
 t_code			token_single(const t_toktype type,
 								t_line *line, t_tokenlst *lst);
-t_code			token_str_wrapper(const t_toktype type,
+t_code			token_unknown(const t_toktype type,
 									t_line *line, t_tokenlst *lst);
 t_code			token_newline(t_line *line, t_tokenlst *lst);
 t_code			token_quote(const t_toktype type,
@@ -36,9 +30,19 @@ t_code			token_quote(const t_toktype type,
 								t_tokenlst *lst);
 
 /*
+** Lexer Checker
+*/
+
+t_code			lexer_checker(t_tokenlst *lst);
+void			find_labels_pass1(t_token *token);
+void			find_labels_pass2(t_token *token);
+void			find_opcodes(t_token *token);
+t_op			*find_op(char *string);
+
+/*
 ** Errors
 */
 
-t_code			error_handler(t_errors error, t_toktype *token, t_line *line);
+t_code			error_handler(t_errors error, t_token *token, t_line *line);
 
 #endif
