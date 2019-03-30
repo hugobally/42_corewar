04368c7b83f3d3b5d431613ed1b0ba3cfd08e7c3
hbally <hbally@student.42.fr>
2019-03-30 18:21:43 +0100

    asm | WIP - added lexer_checker

---
+++ asm_module/src/lexer_checker.c
@@ -0,0 +1,30 @@
+#include "types.h"
+#include "asm.h"
+
+#include "libft.h"//DEBUG
+
+/*
+** Lexer -> Pass 2 - IDENTIFIER : Determine what each unknown token is
+**								  and identify errors
+*/
+
+void			tokens_foreach(t_tokenlst *lst, void (*action)(t_token*))
+{
+	t_token		*node;
+
+	node = lst->start;
+	while (node)
+	{
+		action(node);
+		node = node->next;
+	}
+}
+
+t_code			lexer_checker(t_tokenlst *lst)
+{
+	tokens_foreach(lst, &find_labels_pass1);
+	tokens_foreach(lst, &find_labels_pass2);
+	tokens_foreach(lst, &find_opcodes);
+	debug_printtokenlst(lst);
+	return (done);
+}
