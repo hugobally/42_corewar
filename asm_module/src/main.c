795bed92b14a80330fb780f1c22d8a0350afe53d
hbally <hbally@student.42.fr>
2019-03-30 14:04:43 +0100

    asm | main : minor

--- asm_module/src/main.c
+++ asm_module/src/main.c
@@ -7,7 +7,10 @@ int					main(int argc, char **argv)
 	//TEMPORARY
 	int fd;
 	(void)argc;
-	fd = open(argv[1], O_RDONLY);
+	if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		fd = 0;
 	lexer_scanner(fd);
 	//END TEMPORARY
 }
