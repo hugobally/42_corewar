84376651407aeb8a225cad5db9fc9da7bf8a3252
Yllab <bally.hugo@gmail.com>
2019-03-31 14:22:49 +0200

    asm | lexer_exit : added token_del

--- asm_module/src/lexer_exit.c
+++ asm_module/src/lexer_exit.c
@@ -1,4 +1,20 @@
 #include "types.h"
+#include "libft.h"
+
+void			token_del(t_token *token, t_tokenlst *lst)
+{
+	if (token->value)
+		ft_memdel((void**)&(token->value));
+	if (token->previous)
+		token->previous->next = token->next;
+	if (!token->next)
+		lst->end = token->previous;
+	if (!token->previous)
+		lst->start = token->next;
+	token->next = NULL;
+	token->previous = NULL;
+	ft_memdel((void**)&(token));
+}
 
 t_code			lexer_exit(t_tokenlst *lst, t_errors code)
 {
