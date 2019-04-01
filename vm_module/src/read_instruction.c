fda89c003807fc4c965210403770470a14ad7906
Rodolphe LE RU <rle-ru@e1r2p9.42.fr>
2019-04-01 16:14:44 +0200

    read instruction ok

--- vm_module/src/read_instruction.c
+++ vm_module/src/read_instruction.c
@@ -1,8 +1,91 @@
 #include "corewar.h"
 
-void		get_params(t_core *core, t_process *pro)
+void		get_params3(t_core *core, t_process *pro, uint32_t i, uint8_t code)
 {
+	if (T_IND & g_op_tab[pro->instruction - 1].param_types[2])
+		if (code & IND_CODE)
+			pro->params.p1 = (int16_t)core->arena[i];
+	if (T_DIR & g_op_tab[pro->instruction - 1].param_types[2])
+		if (code & DIR_CODE)
+			pro->params.p1 = (int32_t)core->arena[i];
+	if (T_REG & g_op_tab[pro->instruction - 1].param_types[2])
+		if (code & REG_CODE)
+			pro->params.p1 = (int32_t)core->arena[i];
+	if (code & T_IND)
+	{
+		pro->instruction_size += IND_SIZE;
+		i += IND_SIZE;
+	}
+	else if (code & T_DIR)
+	{
+		pro->instruction_size += DIR_SIZE;
+		i+= DIR_SIZE;
+	}
+	return (i);
+}
 
+int			get_params2(t_core *core, t_process *pro, uint32_t i, uint8_t code)
+{
+	if (T_IND & g_op_tab[pro->instruction - 1].param_types[1])
+		if (code & IND_CODE)
+			pro->params.p1 = (int16_t)core->arena[i];
+	if (T_DIR & g_op_tab[pro->instruction - 1].param_types[1])
+		if (code & DIR_CODE)
+			pro->params.p1 = (int32_t)core->arena[i];
+	if (T_REG & g_op_tab[pro->instruction - 1].param_types[1])
+		if (code & REG_CODE)
+			pro->params.p1 = (int32_t)core->arena[i];
+	if (code & T_IND)
+	{
+		pro->instruction_size += IND_SIZE;
+		i += IND_SIZE;
+	}
+	else if (code & T_DIR)
+	{
+		pro->instruction_size += DIR_SIZE;
+		i+= DIR_SIZE;
+	}
+	return (i);
+}
+
+int			get_params1(t_core *core, t_process *pro, uint32_t i, uint8_t code)
+{
+	if (T_IND & g_op_tab[pro->instruction - 1].param_types[0])
+		if (code & IND_CODE)
+			pro->params.p1 = (int16_t)core->arena[i];
+	if (T_DIR & g_op_tab[pro->instruction - 1].param_types[0])
+		if (code & DIR_CODE)
+			pro->params.p1 = (int32_t)core->arena[i];
+	if (T_REG & g_op_tab[pro->instruction - 1].param_types[0])
+		if (code & REG_CODE)
+			pro->params.p1 = (int32_t)core->arena[i];
+	if (code & T_IND)
+	{
+		pro->instruction_size += IND_SIZE;
+		i += IND_SIZE;
+	}
+	else if (code & T_DIR)
+	{
+		pro->instruction_size += DIR_SIZE;
+		i+= DIR_SIZE;
+	}
+	return (i);
+}
+
+void		get_params(t_core *core, t_process *pro, uint32_t i)
+{
+	uint8_t		code;
+
+	code = pro->params.bytecode >> 6;
+	if (!code)
+	{
+		pro->instruction = 0;
+		pro->instruction_size = 1;
+		return ;
+	}
+	i = get_params1(core, pro, i, pro->params.bytecode >> 6);
+	i = get_params2(core, pro, i, pro->params.bytecode >> 4);
+	get_params3(core, pro, i, pro->params.bytecode >> 2);
 }
 
 void		read_instructions(t_core *core, t_process *pro)
@@ -18,7 +101,7 @@ void		read_instructions(t_core *core, t_process *pro)
 	{
 		pro->bytecode = core->arena[i++];
 		++pro->instruction_size;
-		get_params(core, pro);
+		get_params(core, pro, i);
 	}
 	else
 	{
