5bbb7b9f11beaebc61bcf14d35f8d9406d83fc0e
hbally <hbally@student.42.fr>
2019-04-03 19:32:58 +0200

    asm | WIP

--- asm_module/test/script.sh
+++ asm_module/test/script.sh
@@ -5,3 +5,4 @@
 sed -i '' -e "s/TEST_DIR/1768515945/g" tmp.s
 sed -i '' -e "s/TEST_IND/13398/g" tmp.s
 sed -i '' -e "s/TEST_REG/r15/g" tmp.s
+echo mine --------- ; ./asm tmp.s ; hexdump tmp.cor ; cp tmp.cor mine.cor ; echo --------- ; echo ref ------- ; ./asm_ref tmp.s ; hexdump tmp.cor ; echo ---- ; diff mine.cor tmp.cor
