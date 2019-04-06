33ef1d232edcde45d95381d4bb94670baf0b33a1
Yllab <bally.hugo@gmail.com>
2019-04-06 16:57:54 +0200

    asm | added support for hex

--- asm_module/src/lexer_instructions.c
+++ asm_module/src/lexer_instructions.c
@@ -58,25 +58,47 @@ void				find_commands(t_token *token)
 	}
 }
 
+uint8_t				check_hex_value(t_token *token)
+{
+	int				i;
+
+	i = 2;
+	if (token->value[0] == '0' && token->value[1]
+			&& ft_strchr("xX", token->value[1]))
+	{
+		while (token->value[i]
+				&& ft_strchr("0123456789AaBbCcDdEeFf", token->value[i]))
+			i++;
+		if (i == 2 || token->value[i])
+			error_handler(unknown_token, token, 0);
+		return (1);
+	}
+	else 
+		return (0);
+}
+
 void				find_num(t_token *token)
 {
 	int				i;
 
 	if (token->type == unknown)
 	{
-		i = token->value[0] != '-' ? 0 : 1;
-		if (!ft_isdigit(token->value[i]))
-			error_handler(unknown_token, token, 0);
-		else
-		{
-			while (ft_isdigit(token->value[i]))
-				i++;
-			if (token->value[i])
-				error_handler(unknown_token, token, 0);
-		}
 		if (token->previous && token->previous->type == char_dir)
 			token->type = dir_num;
 		else
 			token->type = ind_num;
+		if (!check_hex_value(token))
+		{
+			i = token->value[0] != '-' ? 0 : 1;
+			if (!ft_isdigit(token->value[i]))
+				error_handler(unknown_token, token, 0);
+			else
+			{
+				while (ft_isdigit(token->value[i]))
+					i++;
+				if (token->value[i])
+					error_handler(unknown_token, token, 0);
+			}
+		}
 	}
 }
