5ea4fa19215fef4a09d5d33bfac5c717e77c3398
Rodolphe LE RU <rle-ru@e2r5p17.42.fr>
2019-04-16 14:51:51 +0200

    moved get_params

--- vm_module/src/instructions_2.c
+++ vm_module/src/instructions_2.c
@@ -121,22 +121,10 @@ t_errors	ft_xor(t_core *core, t_process *process)
 	int			pa[3];
 
 	ft_bzero(pa, sizeof(int) * 3);
-	p1 = 0;
-	p2 = 0;
 	if ((ret = ft_reg(process, &pa[0], &pa[1], &pa[2])) != ok)
 		return (ok);
-	if (pa[0] == REG)
-		p1 = process->regs[process->params.p1 - 1];
-	else if (pa[0] == DIR)
-		p1 = process->params.p1;
-	else if (pa[0] == IND)
-		p1 = core->arena[get_pc(process->pc + (process->params.p1 % IDX_MOD))];
-	if (pa[1] == REG)
-		p2 = process->regs[process->params.p2 - 1];
-	else if (pa[1] == DIR)
-		p2 = process->params.p2;
-	else if (pa[1] == IND)
-		p2 = core->arena[get_pc(process->pc + (process->params.p2 % IDX_MOD))];
+	p1 = ft_get_params(core, process, process->params.p1, pa[0]);
+	p2 = ft_get_params(core, process, process->params.p2, pa[1]);
 	process->regs[process->params.p3 - 1] = p1 ^ p2;
 	ft_carry(process, process->regs[process->params.p3 - 1]);
 	return (ok);
