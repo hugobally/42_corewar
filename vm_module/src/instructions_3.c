c506cb4d1ba37ae5a45bb75e9d2a551cdd7a0328
Rodolphe LE RU <rle-ru@e1r3p8.42.fr>
2019-04-18 13:44:44 +0200

    instructions_3 norme

--- vm_module/src/instructions_3.c
+++ vm_module/src/instructions_3.c
@@ -84,65 +84,27 @@ t_errors		ft_ldi(t_core *core, t_process *process)
 
 t_errors			ft_sti(t_core *core, t_process *process)
 {
-	// t_params	params;
-
-	// int			p1;
-	// int			p2;
-	// int			p3;
-	// int			pa[3];
-
-	// p1 = 0;
-	// p2 = 0;
-	// ft_bzero(pa, sizeof(int) * 3);
-	// params = process->params;
-	// if (ft_reg(process, &pa[0], &pa[1], &pa[2]) != ok)
-	// 	return (ok);
-	// if (pa[1] == REG)
-	// 	p1 = process->pc + ;
-	// else if (pa[1] == DIR)
-	// else if (pa[1] == IND)
-	// if (pa[2] == REG)
-	// else if (pa[2] == DIR)
-
-	// p3 = get_pc(process->pc + (p1 + p2) % IDX_MOD);
-	// write_val(core, p3);
-
-
-	t_params	params;
 	int			p1;
 	int			p2;
-	int			p3;
-	int 		ret;
+	int			pa[3];
 
-	//ft_printf("ft_sti IN by %d\n", process->regs[0]);
-	params = process->params;
-	//ft_printf("params p1:%d, p2:%d, p3:%d, bytecode:%x, pc:%d\n", params.p1, params.p2, params.p3, params.bytecode, process->pc);
-	//ft_printf("registre de p1 %d\n", process->regs[params.p1 - 1]);
-	if ((ret = ft_reg(process, &p1, &p2, &p3)) != ok)
+	p1 = 0;
+	p2 = 0;
+	ft_bzero(pa, sizeof(int) * 3);
+	if (ft_reg(process, &pa[0], &pa[1], &pa[2]) != ok)
 		return (ok);
-	if (p2 == REG)
-	{
-		if (p3 == REG)
-			write_val(core, get_pc(process->pc + (process->regs[params.p2 - 1] + process->regs[params.p3 - 1]) % IDX_MOD), 4, process->regs[params.p1 - 1], process->player);
-		if (p3 == DIR)
-			write_val(core, get_pc(process->pc + (process->regs[params.p2 - 1] + params.p3) % IDX_MOD), 4, process->regs[params.p1 - 1], process->player);
-	}
-	if (p2 == DIR)
-	{
-		if (p3 == REG)
-			write_val(core, get_pc(process->pc + (params.p2 + process->regs[params.p3 - 1]) % IDX_MOD), 4, process->regs[params.p1 - 1], process->player);
-		if (p3 == DIR)
-			write_val(core, get_pc(process->pc + (params.p2 + params.p3) % IDX_MOD), 4, process->regs[params.p1 - 1], process->player);
-	}
-	if (p2 == IND)
-	{
-		if (p3 == REG)
-			write_val(core, get_pc(process->pc + (read_val(core, get_pc(process->pc + params.p2 % IDX_MOD), 4) + process->regs[params.p3 - 1]) % IDX_MOD), 4, process->regs[params.p1 - 1], process->player);
-		if (p3 == DIR)
-			write_val(core, get_pc(process->pc + (read_val(core, get_pc(process->pc + params.p2 % IDX_MOD), 4) + params.p3) % IDX_MOD), 4, process->regs[params.p1 - 1], process->player);
-	}
-	//ft_printf("ft_sti OUT by %d\n", process->regs[0]);
-	return (ok);
+	if (pa[1] == REG)
+		p1 = process->regs[process->params.p2 - 1];
+	else if (pa[1] == DIR)
+		p1 = process->params.p2;
+	else if (pa[1] == IND)
+		p1 = read_val(core, process->pc + (process->params.p2 % IDX_MOD), 4);
+	if (pa[2] == REG)
+		p2 = process->regs[process->params.p3 - 1];
+	else if (pa[2] == DIR)
+		p2 = process->params.p3;
+	write_val(core, get_pc(process->pc + ((p1 + p2) % IDX_MOD)), 4, process->regs[process->params.p1 - 1], process->player);
+	return (0);
 }
 
 t_errors			ft_fork(t_core *core, t_process *process)
