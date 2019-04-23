e7b9be1019084ef1b2728819fa253c1fb4e57927
Rodolphe LE RU <rle-ru@e1r3p12.42.fr>
2019-04-23 15:02:49 +0200

    corrected usage

--- vm_module/src/ft_usage.c
+++ vm_module/src/ft_usage.c
@@ -6,7 +6,7 @@
 /*   By: rle-ru <rle-ru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 14:42:24 by rle-ru            #+#    #+#             */
-/*   Updated: 2019/04/23 15:00:32 by rle-ru           ###   ########.fr       */
+/*   Updated: 2019/04/23 15:02:22 by rle-ru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_usage(char *exe_name)
 	ft_printf(
 	"Usage: %s [-s N -dump N -d N -v N | -visu | -a] <champion1.cor> <...>\n",
 	exe_name);
+	ft_printf("-visu   : Ncurses output mode\n");
 	ft_printf("-a      : Prints output from \"aff\" (Default is to hide it)\n");
 	ft_printf("## TEXT MODE ##\n");
 	ft_printf(
@@ -28,11 +29,10 @@ void	ft_usage(char *exe_name)
 	ft_printf(
 	"-sdump  : Runs N cycles, dumps memory, pauses, then repeats\n");
 	ft_printf(
-	"-v N    : Verbosity levels, can be added together to enable several");
+	"-v N    : Verbosity levels, can be added together to enable several\n");
 	ft_printf("          - 1 : Show lives\n");
 	ft_printf("          - 2 : Show cycles\n");
 	ft_printf("          - 4 : Show operations\n");
 	ft_printf("          - 8 : Show deaths\n");
 	ft_printf("          - 16 : Show PC movements\n");
-	ft_printf("-visu   : Ncurses output mode\n");
 }
