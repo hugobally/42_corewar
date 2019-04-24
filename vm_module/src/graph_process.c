71d267cfc3bd1e8d85cc39ebbf3b9aa032767a8a
Trouz <timo.lesven@gmail.com>
2019-04-24 14:10:58 +0200

    add instruction title and norme

--- vm_module/src/graph_process.c
+++ vm_module/src/graph_process.c
@@ -6,7 +6,7 @@
 /*   By: tlesven <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 14:01:27 by tlesven           #+#    #+#             */
-/*   Updated: 2019/04/23 15:38:03 by tlesven          ###   ########.fr       */
+/*   Updated: 2019/04/24 14:10:31 by tlesven          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,27 @@ void	print_process(t_graph *g, t_process *p)
 	int			i;
 	int			col;
 	int			row;
+	const char	*instr[18] = {"error", "live", "load", "store", "add",
+		"sub", "and", "or", "xor", "zjmp", "ldi", "sti", "forky", "lid",
+		"lldi", "lforky", "aff"};
 
 	i = 1;
 	tmp = p;
 	while (tmp)
-	{//CHANGER L"ACTION
+	{
 		get_col_row(tmp->pc, &col, &row);
 		if (tmp == g->selected_proc)
 			wattron(g->pro_win, COLOR_PAIR(tmp->player + PLAYER_DIFF + 4));
 		else
 			wattron(g->pro_win, COLOR_PAIR(tmp->player + PLAYER_DIFF));
-		mvwprintw(g->pro_win, i, 2, "#%-5d%-2.2dx%-2.2d [%c][%c]%5d in%4d laps.",
-			i, row - 1, col - 2, tmp->is_alive ? 'L' : ' ', 
-			tmp->carry ? 'C' : ' ', tmp->instruction, tmp->remaining_cycles);
+		mvwprintw(g->pro_win, i, 2,
+			"#%-5d%-2.2dx%-2.2d [%c][%c]%7s in%4d laps.",
+			i, row - 1, col - 2, tmp->is_alive ? 'L' : ' ',
+			tmp->carry ? 'C' : ' ', instr[tmp->instruction],
+			tmp->remaining_cycles);
 		tmp = tmp->next;
 		++i;
 	}
-	//wrefresh(g->pro_win);
 }
 
 void	create_process_win(t_graph *g, t_process *p)
@@ -42,5 +46,4 @@ void	create_process_win(t_graph *g, t_process *p)
 	g->pro_win = create_new_win(PRO_ROW, PRO_COL, ARENA_COL, 0);
 	mvwprintw(g->pro_win, 0, (PRO_COL / 2) - 7, " - PROCESS - ");
 	print_process(g, p);
-//	wrefresh(g->pro_win);
 }
