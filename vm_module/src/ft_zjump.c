8727e14811d82484084d256b324c8849244af9cd
Adrien LAC <alac@e1r2p9.42.fr>
2019-04-24 17:14:40 +0200

    zjmp

--- vm_module/src/ft_zjump.c
+++ vm_module/src/ft_zjump.c
@@ -15,7 +15,7 @@ t_errors		ft_zjmp(t_core *core, t_process *process)
 		if (core->visu)
 			move_proccess_on_arena(process->pc,
 			process->pc + process->params.p1, core->graph);
-		process->pc = process->pc + process->params.p1 % IDX_MOD;
+		process->pc = get_pc(process->pc + process->params.p1 % IDX_MOD);
 	}
 	return (ok);
 }
