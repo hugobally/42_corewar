fff8ee6cb4fa434dd5fc836cb9938a6e2d594936
hbally <hbally@student.42.fr>
2019-04-29 15:24:44 +0200

    test | minor

--- test/auto.py
+++ test/auto.py
@@ -13,9 +13,6 @@ max_children = int(subprocess.getoutput("cat auto/auto.conf | grep NUM_PROCESSES
 dir = os.listdir(COR_DIR)
 shuffle(dir)
 
-print (dir)
-exit()
-
 total = 0
 for i in range(0, len(dir)):
     for j in range(i + 1, len(dir)):
@@ -27,7 +24,9 @@ num_children = 0
 for i in range(0, len(dir)):
     for j in range(i + 1, len(dir)):
         while num_children >= max_children:
-            time.sleep(10)
+            time.sleep(1)
             num_children = int(subprocess.getoutput("ps -ef | grep auto_corewar | grep -v grep | wc -l"))
         os.system("./auto/auto_corewar " + COR_DIR + dir[i] + " " + COR_DIR + dir[j] + " &")
         num_children = int(subprocess.getoutput("ps -ef | grep auto_corewar | grep -v grep | wc -l"))
+
+os.system("./auto/auto_check")
